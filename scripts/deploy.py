138ea43ae23bf0ad06a58c7f377b27ca2f995e3f
Victor Lapa <victtorlapa1@gmail.com>
2022-01-27 20:02:59 -0300

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,16 @@
+from brownie import FundMe, network
+from scripts.utils import get_account
+
+
+def deploy_fund_me():
+    account = get_account()
+    fund_me = FundMe.deploy(
+        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        {"from": account},
+        publish_source=True,
+    )
+    print(f"Contract deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
